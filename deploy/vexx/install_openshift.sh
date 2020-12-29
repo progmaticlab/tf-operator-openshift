71c1abc761dd6099483303d15adcc4b06c72dad4
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-12-29 14:23:19 +0300

    python -> python3

--- deploy/vexx/install_openshift.sh
+++ deploy/vexx/install_openshift.sh
@@ -185,7 +185,7 @@ EOF
 
 for index in $(seq 0 2); do
     MASTER_HOSTNAME="$INFRA_ID-master-$index\n"
-    python -c "import base64, json, sys;
+    python3 -c "import base64, json, sys;
 ignition = json.load(sys.stdin);
 files = ignition['storage'].get('files', []);
 files.append({'path': '/etc/hostname', 'mode': 420, 'contents': {'source': 'data:text/plain;charset=utf-8;base64,' + base64.standard_b64encode(b'$MASTER_HOSTNAME').decode().strip(), 'verification': {}}, 'filesystem': 'root'});
