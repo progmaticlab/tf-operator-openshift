b52ec21abf3b503b2ea7ba05802461104dc2b2c9
Ivan Timofeev <ivanv.timofeev@gmail.com>
2021-02-04 17:10:19 +0300

    Couple configs more

--- deploy/kvm/install_openshift.sh
+++ deploy/kvm/install_openshift.sh
@@ -11,13 +11,16 @@ err() {
     exit 1
 }
 
+[[ -z ${OPENSHIFT_PULL_SECRET} ]] || err "set OPENSHIFT_PULL_SECRET env variable"
+[[ -z ${OPENSHIFT_PUB_KEY} ]] || err "set OPENSHIFT_PUB_KEY env variable"
+
 OCP_VERSION=${OCP_VERSION:-"4.5.21"}
 RHCOS_VERSION=${RHCOS_VERSION:="4.6/4.6.8"}
 RHCOS_IMAGE="rhcos-metal.x86_64.raw.gz"
 RHCOS_KERNEL="rhcos-live-kernel-x86_64"
 RHCOS_INITRAMFS="rhcos-live-initramfs.x86_64.img"
-N_MAST=${N_MAST:-"3"}
-N_WORK=${N_WORK:-"2"}
+N_MASTER=${N_MAST:-"3"}
+N_WORKER=${N_WORK:-"2"}
 MASTER_CPU=${MASTER_CPU:-"4"}
 MASTER_MEM=${MASTER_MEM:-"16000"}
 WORKER_CPU=${WORKER_CPU:-"2"}
@@ -77,5 +80,46 @@ fi
 if [[ ! -f ${DOWNLOADS_DIR}/${LB_IMAGE} ]]; then
     wget "$LB_IMG_URL" -O "${DOWNLOADS_DIR}/$LB_IMAGE"
 fi
+
+
+mkdir -p rhcos-install
+cp "${DOWNLOADS_DIR}/${RHCOS_KERNEL}" "rhcos-install/vmlinuz"
+cp "${DOWNLOADS_DIR}/${RHCOS_INITRAMFS}" "rhcos-install/initramfs.img"
+cat <<EOF > rhcos-install/.treeinfo
+[general]
+arch = x86_64
+family = Red Hat CoreOS
+platforms = x86_64
+version = ${OCP_VER}
+[images-x86_64]
+initrd = initramfs.img
+kernel = vmlinuz
+EOF
+
+cat <<EOF > ${INSTALL_DIR}/install-config.yaml
+apiVersion: v1
+baseDomain: ${BASE_DOMAIN}
+compute:
+- hyperthreading: Disabled
+  name: worker
+  replicas: 0
+controlPlane:
+  hyperthreading: Disabled
+  name: master
+  replicas: ${N_MASTER}
+metadata:
+  name: ${CLUSTER_NAME}
+networking:
+  clusterNetworks:
+  - cidr: 10.128.0.0/14
+    hostPrefix: 23
+  networkType: OpenShiftSDN
+  serviceNetwork:
+  - 172.30.0.0/16
+platform:
+  none: {}
+pullSecret: '${OPENSHIFT_PULL_SECRET}'
+sshKey: '${OPENSHIFT_PUB_KEY}'
+EOF
 
 
