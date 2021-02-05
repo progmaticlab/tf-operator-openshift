93c1145cb9631eeb6f2caecb7e9a341d250b4efe
Ivan Timofeev <ivant.timofeev@gmail.com>
2021-02-05 13:35:53 +0000

    Fix an error

--- deploy/kvm/destroy_cluster.sh
+++ deploy/kvm/destroy_cluster.sh
@@ -6,4 +6,4 @@ sudo virsh destroy ${CLUSTER_NAME}-lb || /bin/true
 sudo virsh undefine ${CLUSTER_NAME}-lb --remove-all-storage || /bin/true
 sed_cmd=$(echo "/${CLUSTER_NAME}\.${BASE_DOMAIN}/d")
 sudo sed -i_bak -e ${sed_cmd} /etc/hosts
-sudo sed -i_bak -e 'xxxtestxxx' /etc/hosts
+sudo sed -i_bak -e "/xxxtestxxx/d" /etc/hosts
