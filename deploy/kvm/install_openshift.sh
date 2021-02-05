6f234e65a14223b790f95a545d3d3a9c4d01aa1b
Ivan Timofeev <ivant.timofeev@gmail.com>
2021-02-05 10:27:32 +0000

    Add sudo

--- deploy/kvm/install_openshift.sh
+++ deploy/kvm/install_openshift.sh
@@ -228,7 +228,7 @@ sudo virsh start ${CLUSTER_NAME}-lb
 
 while true; do
     sleep 5
-    LBIP=$(virsh domifaddr "${CLUSTER_NAME}-lb" | grep ipv4 | head -n1 | awk '{print $4}' | cut -d'/' -f1 2> /dev/null)
+    LBIP=$(sudo virsh domifaddr "${CLUSTER_NAME}-lb" | grep ipv4 | head -n1 | awk '{print $4}' | cut -d'/' -f1 2> /dev/null)
     test "$?" -eq "0" -a -n "$LBIP"  && { echo "$LBIP"; break; }
 done
 MAC=$(virsh domifaddr "${CLUSTER_NAME}-lb" | grep ipv4 | head -n1 | awk '{print $2}')
